a9573ebb72e47180964b0ded164a9f462bc7425f
Akshit <akshit.garg.cer21@itbhu.ac.in>
2023-07-05 21:23:57 +0530

    mm

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,32 +1,38 @@
-import { createContext, useEffect, useReducer } from "react";
+import {createContext, useReducer} from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user:JSON.parse(localStorage.getItem("user")) || null,
-  isFetching: false,
-  error: false,
+    user: 
+    // null,
+    {
+    _id: "645298e33d26689252d9dc19",
+    username:"Akshit",
+    email:"akshit@gmail.com",
+    profilePicture: "person/1.jpeg",
+    coverPicture:"",
+    isAdmin: false,
+    followers: [],
+    followings: [1,2],
+    },
+    isFetching: false,
+    error: false,
 };
-
 
 export const AuthContext = createContext(INITIAL_STATE);
 
-export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
-  
-  useEffect(()=>{
-    localStorage.setItem("user", JSON.stringify(state.user))
-  },[state.user])
-  
-  return (
-    <AuthContext.Provider
-      value={{
-        user: state.user,
-        isFetching: state.isFetching,
-        error: state.error,
-        dispatch,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+export const AuthContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+    return (
+        <AuthContext.Provider 
+        value={{
+            user: state.user, 
+            isFetching: state.isFetching, 
+            error: state.error, 
+            dispatch,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
